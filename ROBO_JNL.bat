92b56cd836a719e4e6a824a1d0908f0598fb32c2
Arthur JNL <seu-email@jnl.com.br>
2026-03-12 16:45:35 -0300

    Limpeza Suprema: Apenas arquivos do sistema

--- ROBO_JNL.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\Arthur\AppData\Local\Python\pythoncore-3.14-64\python.exe" "C:\Users\Arthur\OneDrive - JNL COMÉRCIO EXTERIOR LTDA\Documentos\AUTOMAÇÕES\automacao_jnl_contas a receber (PIX).py"
-pause
